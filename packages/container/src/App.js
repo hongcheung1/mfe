072b0dddcbe03dde909eb63364504142b367cc26
ljacobs <lryjacobs@gmail.com>
2022-10-09 20:49:29 -0700

    fix typos

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import MarketingApp from './components/MarketingApp';
 import Header from './components/Header';
-import { StylesProvider, createGenrateClassName } from '@material-ui/core/styles';
+import { createGenerateClassName, StylesProvider } from '@material-ui/core/styles';
 
 const generateClassName = createGenerateClassName({
   productionPrefix: 'co'
@@ -11,7 +11,7 @@ const generateClassName = createGenerateClassName({
 export default () => {
   return (
     <BrowserRouter>
-      <StylesProvider generateClassName={generateClassname}>
+      <StylesProvider generateClassName={generateClassName}>
         <div>
           <Header/>
           <MarketingApp />
